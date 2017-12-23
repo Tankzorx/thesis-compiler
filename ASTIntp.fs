e4a12466e144b00b22c90366db14d0ba3ea63aac
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-12-23 23:49:53 +0100

    A bit of tests for intpDp

--- ASTIntp.fs
+++ ASTIntp.fs
@@ -7,6 +7,10 @@ open Zorx.Frontend.AST
 
 module Interpreter =
 
+    type FsmNextStateResult =
+            | Success of string * string list * Exp
+            | Error of string
+
     let findTransitionsByStartState tList state =
         List.filter (fun t -> 
             let (T (stateName, _, _, _)) = t
@@ -27,27 +31,71 @@ module Interpreter =
 
         // let actionNames = List.map (fun x -> x) actL
         let fsmFunc = intpFsm fsm
-        printfn "doneski"
         fsmFunc
     
+    and intpStm (stm: Stm, ctx: Map<string, Const>)  =
+        let (Ass (lval, exp)) = stm
+        let value = intpExp exp ctx
+        ctx.Add(lval, value)
+
     // Should return nextState function. input: state, cIn, ss
     // Output: next state, control signal(action list), csOut
     and intpFsm (fsm: Fsm) =
         let (Fsm (decL, tL)) = fsm
-        let retVal (state: string, (inputVector: Map<string, Const>)) =
-            // possible by start state
-            let possibleTransitions = findTransitionsByStartState tL state
-            let transitions = 
+
+        let retVal (state: string, (inputVector: Map<string, Const>)): FsmNextStateResult =
+
+            let validTransitions =
+                findTransitionsByStartState tL state |>
                 List.filter (fun t -> 
                     let (T (s1, exp, actions, s2)) = t
                     let v = intpExp exp inputVector
                     v = B true
-                ) possibleTransitions
-            failwith "NYI"
-        // failwith "NYI"
+                ) 
+
+            if validTransitions.Length > 1 then
+                let errMsg = (sprintf "Undeterministic automaton. Can pick between: %A" validTransitions)
+                Error errMsg
+            elif validTransitions.Length = 0 then
+                let errMsg = sprintf "No transition at: %A" state
+                Error errMsg
+            else
+                let (T (s1, exp, actions, s2)) = validTransitions.Head
+                Success (s2, actions, C (B false))
         retVal
-                
     
+    // Should return datapath function.
+    // dp function has the following pseudo signature:
+    // input: actionList, dpInput, ctx/registerState
+    // output: status signals, new register state, dpOut
+    and intpDp (dp: Datapath) =
+        let (Datapath (decL, actionL)) = dp
+        let getActionByName actionName = 
+            let rec inner actionLs =
+                match actionLs with
+                    | [] -> failwith (sprintf "No action matched the requested action: %A" actionName)
+                    | a::tail ->
+                        let (Action (name, stms)) = a
+                        if name = actionName then
+                            a
+                        else
+                            inner tail
+            inner actionL
+
+        let retVal (actionList: string list, ctx: Map<string, Const>) =
+            let oldRegisterState = ctx
+            List.fold (fun acc actionName ->
+                // printfn "I'm folding, folding, folding.. %A" actionName
+                let action = getActionByName actionName
+                intpAction (action, oldRegisterState)
+            ) ctx actionList
+
+        retVal
+
+    and intpAction (action: Action, ctx: Map<string, Const>): Map<string, Const> =
+        let (Action (name, stmL)) = action
+        List.fold (fun acc stm  -> intpStm (stm, acc)) ctx stmL
+
     and intpExp (exp: Exp) ctx: Const =
         // failwith "NYI"
         match exp with
@@ -84,8 +132,8 @@ module Interpreter =
             | _ -> failwith (sprintf "NYI interpret for: '%A'" exp)
 
     and intpAccess (access: Access) (ctx: Map<string, Const>) =
-        printfn "sddsdad"
         let (AVar s) = access
+        printfn "Accessing: %s" s
         ctx.[s]
 
 
--- main.fsx
+++ main.fsx
@@ -13,7 +13,6 @@ open Zorx.Util
 open Zorx.Interpreter
 open Zorx.Frontend.AST
 
-// parseFromFile "test/lexingTest.zorx"
 let ast = parseFromFile "test/findLargest.zorx"
 printfn "%A" ast
 let (S ms) = ast
@@ -22,16 +21,35 @@ match ms with
     | m::ms ->
         let (M (name, fsm, dp)) = m
         let (Fsm (decL, tL)) = fsm
-        printfn "asd"
         match tL with
             | [] -> failwith "Empty transition list"
             | t::tls -> 
-                printfn "30"
                 let (T (s1, exp, actionList, s2)) = t
-                printfn "31"
                 let v = intpExp exp (Map.empty.Add("cIn", B true))
-                printfn "33"
-                printfn "%A" v
+                printfn "%A" (v = (B true))
+        let nextStateFunction = intpFsm fsm
+        let dummyMap = Map.empty.Add("cIn", B false).Add("newLargestFound", B false)
+        let nextStateRes = nextStateFunction ("Idle", dummyMap)
+        
+        match nextStateRes with
+            | FsmNextStateResult.Success (state',_,_) ->
+                printfn "%A" (state' = "Idle")
+            | FsmNextStateResult.Error s -> printfn "false"
+
+        let dpFunction = intpDp dp
+        let dummyCtx = Map.empty.Add("dpIn", N 3)
+        printfn "%A" dummyCtx
+        let cycle1 = dpFunction (["Init"], dummyCtx)
+        printfn "%A" cycle1
+        let cycle1 = cycle1.Add("dpIn", N 4)
+        let cycle2 = dpFunction (["ReadInput"], cycle1)
+        printfn "%A" cycle2
+        let cycle2 = cycle2.Add("dpIn", N 5)
+        let cycle3 = dpFunction (["ReadInput"; "SetNewLargest"], cycle2)
+        printfn "%A" cycle3
+        
+
+        
 // intpExp 
 // let dpInVector = Map.empty.Add("dpIn", 3)
 // let findLargestFsmd: (string * Map<string, PrimTyp> -> int) = intpSpecification(ast)
